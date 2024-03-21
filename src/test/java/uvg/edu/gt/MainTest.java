6d2ccb81ed078d1b0cce70e41a37abe4f16da085
Josero31 <san231221@uvg.edu.gt>
2024-03-21 09:58:54 -0600

    cambios

---
+++ src/test/java/uvg/edu/gt/MainTest.java
@@ -0,0 +1,18 @@
+package uvg.edu.gt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+        @org.junit.jupiter.api.Test
+        void main() {
+        }
+
+        @org.junit.jupiter.api.Test
+        void ejecutarOperacionesDesdeArchivo() {
+        }
+
+        @org.junit.jupiter.api.Test
+        void ingresarOperacionesManualmente() {
+        }
+}
---
+++ src/test/java/uvg/edu/gt/QUOTETest.java
@@ -0,0 +1,15 @@
+package uvg.edu.gt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QUOTETest {
+
+        @org.junit.jupiter.api.Test
+        void quote() {
+            String imp = "(QUOTE (A B C D E F G)";
+            String expected = "(A B C D E F G )";
+            String result = QUOTE.quote(imp);
+            assertEquals(expected, result);
+        }
+
+}
